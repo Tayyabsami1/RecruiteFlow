abea41fc3ccce23fad5aa3e7bbe912229f553a2b
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2025-06-11 21:48:04 +0500

    Deployment updates

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,26 @@
-import { app } from "./app.js";
+
 import dotenv from "dotenv/config";
+import serverless from "serverless-http";
 import connectDB from "./DB/index.js";
-import serverless from "serverless-http"
+import { app } from "./app.js";
+
+// Vercel calls this function per request, so we connect DB outside
+let isConnected = false;
+
+async function bootstrapApp() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+
+  return serverless(app);
+}
+
+export default async function handler(req, res) {
+  const server = await bootstrapApp();
+  return server(req, res);
+}
+
 
 // connectDB().then(() => {
 //     app.on("error", (err) => {
@@ -14,5 +33,5 @@ import serverless from "serverless-http"
     
 // })
 // .catch((err) => console.log(err));
-await connectDB();
-export const handler = serverless(app)
+// await connectDB();
+// export const handler = serverless(app)
