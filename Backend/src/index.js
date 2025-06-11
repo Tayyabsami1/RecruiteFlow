9ab79a09265806409794a9b2382544cb75b477e1
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2025-06-11 21:43:25 +0500

    Backend update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,18 @@
 import { app } from "./app.js";
 import dotenv from "dotenv/config";
 import connectDB from "./DB/index.js";
+import serverless from "serverless-http"
 
-connectDB().then(() => {
-    app.on("error", (err) => {
-        console.log("Server Error ", err)
-        throw new Error(err);
-    })
-    app.listen(process.env.PORT || 8000, () => {
-        console.log("Server is started on port", process.env.PORT );
-    });
+// connectDB().then(() => {
+//     app.on("error", (err) => {
+//         console.log("Server Error ", err)
+//         throw new Error(err);
+//     })
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log("Server is started on port", process.env.PORT );
+//     });
     
-})
-.catch((err) => console.log(err));
+// })
+// .catch((err) => console.log(err));
+await connectDB();
+export const handler = serverless(app)
