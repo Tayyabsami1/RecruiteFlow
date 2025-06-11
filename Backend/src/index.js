7fc9609ba1727ae9d99f557d54fc5c5034f2a379
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2025-06-11 22:00:02 +0500

    Changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,34 +4,34 @@ import serverless from "serverless-http";
 import connectDB from "./DB/index.js";
 import { app } from "./app.js";
 
-// Vercel calls this function per request, so we connect DB outside
-let isConnected = false;
+// // Vercel calls this function per request, so we connect DB outside
+// let isConnected = false;
 
-async function bootstrapApp() {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
+// async function bootstrapApp() {
+//   if (!isConnected) {
+//     await connectDB();
+//     isConnected = true;
+//   }
 
-  return serverless(app);
-}
+//   return serverless(app);
+// }
 
-export default async function handler(req, res) {
-  const server = await bootstrapApp();
-  return server(req, res);
-}
+// export default async function handler(req, res) {
+//   const server = await bootstrapApp();
+//   return server(req, res);
+// }
 
 
-// connectDB().then(() => {
-//     app.on("error", (err) => {
-//         console.log("Server Error ", err)
-//         throw new Error(err);
-//     })
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log("Server is started on port", process.env.PORT );
-//     });
+connectDB().then(() => {
+    app.on("error", (err) => {
+        console.log("Server Error ", err)
+        throw new Error(err);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Server is started on port", process.env.PORT );
+    });
     
-// })
-// .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
 // await connectDB();
 // export const handler = serverless(app)
